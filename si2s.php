575a7d63f778616dad8ce2f743b5129d7fdb2830
Johan Carre <johan.carre@epitech.eu>
2020-02-28 09:05:10 -0500

    push_03_JOHAN

--- si2s.php
+++ si2s.php
@@ -2,35 +2,57 @@
 
 class Person
 {
+	
 
 	public $photo;
 	public $id_photo;
-	public $place;
 	public $prenom;
 	public $fonction;
 	
-	public function __construct( $photo, $id_photo, $place, $prenom, $fonction)
+	public function __construct( $photo, $id_photo, $prenom, $fonction)  #Constructeur crée une personne avec ca photo, son nom... 
 	{
 			$this->photo = $photo;
 			$this->id_photo = $id_photo;
-			$this->place = $place;
 			$this->prenom = $prenom;
 			$this->fonction = $fonction;
 
 
 	}
 }
-$patrick = new Person('pat.PNG', 100, 1, 'Patrick', 'Directeur');
-$olege = new Person('olege.PNG', 101, 2, 'Olege', 'Technicien');
-$johan = new Person('johan.PNG', 102, 2, 'Johan', 'Stagiaire');
 
+# Création des differentes Personnes via le Construteur 
+$patrick = new Person('pat.PNG', 100, 'Patrick', 'Directeur');
+$olege = new Person('olege.PNG', 101, 'Olege', 'Technicien');
+$johan = new Person('johan.PNG', 102, 'Johan', 'Stagiaire');
+$charles = new Person('charles.PNG', 103, 'Charles', 'Technicien');
+$caroline = new Person('caroline.PNG', 104, 'Caroline', 'Secretaire');
+$gabriel = new Person('gabriel.PNG', 105, 'Gabriel', 'Ingenieure');
+$yoann = new Person('yoan.PNG', 106, 'Yoann', 'Technicien');
+$herve = new Person('herve.PNG', 107, 'Herve', 'Ingenieure');
+$nabil = new Person('nabil.PNG', 108, 'Nabil', 'Ingenieure');
+$groupe = new Person('groupe.jpg', 108, 'Groupe Rouge', 'Client SI2S');
+$balibaris = new Person('balibaris.jpg', 108, 'Balibaris', 'Client SI2S');
+$intuitae = new Person('intuitae.jpg', 108, 'Intuitae', 'Client SI2S');
 
+#tableau contenant les personnes
+$tabPerson = array( $patrick, $olege, $johan, $charles, $caroline, $gabriel,$yoann, $herve, $nabil, $groupe, $balibaris, $intuitae );
 
+#Création d'un cercle pour afficher les personnes
 
-echo' 
-<div id="div1" style="width: 200px; height: 150px; top:0; left:700;  position:absolute;"> <img src="'.$patrick->photo.'" alt="patrick"/> <h3>'.$patrick->prenom.'  '.$patrick->fonction.'</h3></div>
-<div id="div2" style="width: 200px; height: 150px; top:300px; left:1000px;  position:absolute;"><img src="'.$olege->photo.'" alt="oleg"/> <h3>'.$olege->prenom.'  '.$olege->fonction.'</h3></div>
-<svg width="1500" height="1500"><line x1="700" y1="00" x2="1500" y2="1350" stroke="black"/></svg>
-';
+$theta_min = 0;
+$theta_max = count($tabPerson) - 1;
+
+$r = 330;
+
+for( $i = $theta_min; $i <= $theta_max; $i++)
+{
+	
+        $x = 370 + $r * cos($i);
+        $y = 850 + $r * sin($i);
+       
+        echo'<div id="div1" style="top:'.$x.'; left:'.$y.';  position:absolute;"> <img style="width: 80px; height: 8
+        0px;" src="'.$tabPerson[$i]->photo.'" alt="patrick"/> <h3>'.$tabPerson[$i]->prenom.'  '.$tabPerson[$i]->fonction.'</h3></div> ';      
+}
+
 ?>
 
