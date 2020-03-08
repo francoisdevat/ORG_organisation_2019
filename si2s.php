38430309f0376cd4a7febb6d8a49dfe965e613e6
Johan Carre <johan.carre@epitech.eu>
2020-03-08 07:18:11 -0400

    dimache 08

--- si2s.php
+++ si2s.php
@@ -16,23 +16,26 @@ class Person
 			$this->prenom = $prenom;
 			$this->fonction = $fonction;
 			$this->buddys = $buddys;
-			$this->$frequency = $frequency;
+			$this->frequency = $frequency;
 	}
 }
 
 # Création des differentes Personnes via le Construteur 
-$patrick = new Person('./items/johan/pat.PNG', 100, 'Patrick', 'Directeur', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$olege = new Person('./items/johan/olege.PNG', 101, 'Olege', 'Technicien', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 10]);
-$johan = new Person('./items/johan/johan.PNG', 102, 'Johan', 'Stagiaire', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$charles = new Person('./items/johan/charles.PNG', 103, 'Charles', 'Technicien', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$caroline = new Person('./items/johan/caroline.PNG', 104, 'Caroline', 'Secretaire', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$gabriel = new Person('./items/johan/gabriel.PNG', 105, 'Gabriel', 'Ingenieure', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$yoann = new Person('./items/johan/yoan.PNG', 106, 'Yoann', 'Technicien', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$herve = new Person('./items/johan/herve.PNG', 107, 'Herve', 'Ingenieure', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$nabil = new Person('./items/johan/nabil.PNG', 108, 'Nabil', 'Ingenieure', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$groupe = new Person('./items/johan/groupe.jpg', 108, 'Groupe Rouge', 'Client SI2S', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$balibaris = new Person('./items/johan/balibaris.jpg', 108, 'Balibaris', 'Client SI2S', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
-$theta_variablentuitae = new Person('./items/johan/intuitae.jpg', 108, 'Intuitae', 'Client SI2S', ['Olege', 'Gabriel', 'Yoann'], [2, 4, 6]);
+$patrick = new Person('./items/johan/pat.PNG', 100, 'Patrick', 'Directeur', ['Olege', 'Gabriel', 'Yoann'], [ [2,1],[4,1] ,[6,2] ]);
+$olege = new Person('./items/johan/olege.PNG', 101, 'Olege', 'Technicien', ['Olege', 'Gabriel', 'Yoann'], [[2,2],[4,3],[10, 2] ]);
+$johan = new Person('./items/johan/johan.PNG', 102, 'Johan', 'Stagiaire', ['Olege', 'Gabriel', 'Yoann'], [[2,2],[4,3],[6,1]]);
+$charles = new Person('./items/johan/charles.PNG', 103, 'Charles', 'Technicien', ['Olege', 'Gabriel', 'Yoann'], [[2,1],[4,3],[6,1]]);
+$caroline = new Person('./items/johan/caroline.PNG', 104, 'Caroline', 'Secretaire', ['Olege', 'Gabriel', 'Yoann'],[ [2, 2],[4, 2],[6,3]]);
+$gabriel = new Person('./items/johan/gabriel.PNG', 105, 'Gabriel', 'Ingenieure', ['Olege', 'Gabriel', 'Yoann'], [[2,3],[4,3],[6,3]]);
+$yoann = new Person('./items/johan/yoan.PNG', 106, 'Yoann', 'Technicien', ['Olege', 'Gabriel', 'Yoann'], [[2,1],[4,1],[6,2]]);
+$herve = new Person('./items/johan/herve.PNG', 107, 'Herve', 'Ingenieure', ['Olege', 'Gabriel', 'Yoann'], [[2, 1],[4, 2],[6,2 ]]);
+$nabil = new Person('./items/johan/nabil.PNG', 108, 'Nabil', 'Ingenieure', ['Olege', 'Gabriel', 'Yoann'], [[2, 3],[4,3 ],[6, 3]]);
+$groupe = new Person('./items/johan/groupe.jpg', 108, 'Groupe Rouge', 'Client SI2S', ['Olege', 'Gabriel', 'Yoann'],[ [2,3 ],[4, 1],[6,1 ]]);
+$balibaris = new Person('./items/johan/balibaris.jpg', 108, 'Balibaris', 'Client SI2S', ['Olege', 'Gabriel', 'Yoann'],[ [2, 2], [4, 1],[6,1 ]]);
+$theta_variablentuitae = new Person('./items/johan/intuitae.jpg', 108, 'Intuitae', 'Client SI2S', ['Olege', 'Gabriel', 'Yoann'], [[2, 2],[4,3 ],[6,1 ]]);
+
+
+
 
 #tableau contenant les personnes
 $tabPerson = array($patrick, $olege, $johan, $charles, $caroline, $gabriel, $yoann, $herve, $nabil, $groupe, $balibaris, $theta_variablentuitae);
@@ -59,10 +62,23 @@ for ($i = 0; $i < count($tabPerson); $i++) {
 	for ($j = 0; $j < count($tabPerson[$i]->buddys); $j++) {
 		for ($k = 0; $k < count($tabPerson); $k++) {
 			if ($tabPerson[$i]->buddys[$j] == $tabPerson[$k]->prenom) {
+				
+				if( $tabPerson[$i]->frequency[0][1] == 1){
+						 $rempl = '1';	
+				}
+				if( $tabPerson[$i]->frequency[0][1] == 2){
+						 $rempl = '3';	
+				}
+				if( $tabPerson[$i]->frequency[0][1] == 3){
+						 $rempl = '6';	
+				}
+				
 				echo'
 					<div id="div2" style="position:absolute">
 					<svg width="500000" height="50000000">
-						<line x1="'.$coords[$k][0].'" y1="'.$coords[$k][1].'" x2="'.$coords[$i][0].'" y2="'.$coords[$i][1].'" style="stroke:rgb(255,0,0);stroke-width:6" />
+					
+						<line x1="'.$coords[$k][0].'" y1="'.$coords[$k][1].'" x2="'.$coords[$i][0].'" y2="'.$coords[$i][1].'" style="stroke:rgb(255,0,0);
+						stroke-width:'.$rempl.'" />
 					</svg>
 					</div>';
 			}
